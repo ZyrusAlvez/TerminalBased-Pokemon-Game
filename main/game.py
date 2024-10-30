3df3b89fe78466c992dbf1fa207440de4bd53078
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2024-10-31 03:36:17 +0800

    pre battle selection added

--- main/game.py
+++ main/game.py
@@ -1,6 +1,7 @@
 from pokemon import Pokemon
 from tabulate import tabulate
 from termcolor import colored
+from utility import clean
 import time
 import os
 import random
@@ -19,7 +20,7 @@ player2 = "Player 2"
 pokemon_list = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6]
 player1_pokemon = []
 player2_pokemon = []
-
+pokemon_table_header = [colored("No.", "yellow", attrs=["bold"]), colored("Name", "yellow", attrs=["bold"]), colored("Type", "yellow", attrs=["bold"]), colored("Health", "yellow", attrs=["bold"]), colored("Power", "yellow", attrs=["bold"])]
 # players will choose pokemon
 i = 0
 while i < 6:
@@ -28,7 +29,7 @@ while i < 6:
     
     # shows list of pokemon
     pokemon_data = [[f"{i}", pokemon.name, pokemon.type, pokemon.health, pokemon.power] for i, pokemon in enumerate(pokemon_list, 1)]
-    print(tabulate(pokemon_data, headers=[colored("No.", "yellow", attrs=["bold"]), colored("Name", "yellow", attrs=["bold"]), colored("Type", "yellow", attrs=["bold"]), colored("Health", "yellow", attrs=["bold"]), colored("Power", "yellow", attrs=["bold"])], tablefmt="github", numalign="center"))
+    print(tabulate(pokemon_data, headers=pokemon_table_header, tablefmt="github", numalign="center"))
         
     # take input
     pick_pokemon = input(f"\n{player} choose your pokemon: ")
@@ -58,10 +59,22 @@ player1_use_potion = False
 player1_use_poison = False
 player2_use_potion = False
 player2_use_poison = False
+player1_used_pokemons = []
+player2_used_pokemons = []
+history = []
+i = 0
 while True:
+    round_history = []
     battle_stats = Pre_Battle_Animation(player1_pokemon, player2_pokemon)
     battle_stats.randomizer_animation()
-
+    
+    if clean(battle_stats.player1_pokemon.name) not in player1_used_pokemons:
+        player1_used_pokemons.append(clean(battle_stats.player1_pokemon.name))
+    if clean(battle_stats.player2_pokemon.name) not in player2_used_pokemons:
+        player2_used_pokemons.append(clean(battle_stats.player2_pokemon.name))
+        
+    round_history.append(battle_stats.round)
+    round_history.append(f"Player 1's {battle_stats.player1_pokemon.name} vs Player's 2 {battle_stats.player2_pokemon.name}")
     player1_temporary_buff = False
     player2_temporary_buff = False
 
@@ -90,16 +103,15 @@ while True:
             print("2. Pokemon")
             print("3. Record")
             print("4. Run")
-            choice = input(f"What will {player_name} do? ")
+            choice = input(f"What will {colored(player_name, 'yellow', attrs=['bold'])} do? ")
             if choice.isdigit():
                 choice = int(choice)
                 if choice in list(range(1, 5)):
                     os.system("cls")
                     battle_stats.last_frame()
                     if choice == 1:
-                        print(f"\nYou randomly sent {player_pokemon.name}\n")
                         while True:   
-                            print("1. Attack")
+                            print("\n1. Attack")
                             print("2. Poison Spell")
                             print("3. Potion Spell")
                             player_action = input(f"{player_pokemon.name} will use: ")
@@ -116,11 +128,45 @@ while True:
                                 battle_stats.last_frame()
                                 print(f"{colored(player_action, 'yellow', attrs=['underline'])} was not a valid keyword\n")
                     if choice == 2:
-                        pass
+                        pokemon_data_player1 = [[f"{i}", pokemon.name, pokemon.type, pokemon.health, pokemon.power] for i, pokemon in enumerate(player1_pokemon, 1)]
+                        pokemon_data_player2 = [[f"{i}", pokemon.name, pokemon.type, pokemon.health, pokemon.power] for i, pokemon in enumerate(player2_pokemon, 1)]
+                        print("")
+                        if player_name == player1:
+                            print(tabulate(pokemon_data_player1, headers=pokemon_table_header, tablefmt="github", numalign="center"))
+                        if player_name == player2:
+                            print(tabulate(pokemon_data_player2, headers=pokemon_table_header, tablefmt="github", numalign="center"))
+                        input("\nPress any key to back: ")
+                        os.system("cls")
+                        battle_stats.last_frame()
                     if choice == 3:
-                        pass
+                        print(tabulate(history, headers=[colored("Round no.", "yellow", attrs=["bold"]), colored("Match", "yellow", attrs=["bold"]), colored("Status", "yellow", attrs=["bold"])], tablefmt="github", numalign="center"))
+                        print(player1_used_pokemons)
+                        print(player2_used_pokemons)
+                        input("\nPress any key to back: ")
+                        os.system("cls")
+                        battle_stats.last_frame()
                     if choice == 4:
-                        pass
+                        if (player_name == player1 and len(player1_used_pokemons) >= 3) or (player_name == player2 and len(player2_used_pokemons) >= 3):
+                            if player_name == player1:
+                                print(player1, "ran away from the battle!\n")
+                            else:
+                                print(player2, "ran away from the battle!\n")
+                            total_player1_health = sum([pokemon.health for pokemon in player1_pokemon])
+                            total_player2_health = sum([pokemon.health for pokemon in player2_pokemon])
+                            if total_player1_health > total_player2_health:
+                                print(f"{colored(player1, 'yellow', attrs=['bold'])} won the battle!")
+                            elif total_player1_health < total_player2_health:
+                                print(f"{colored(player2, 'yellow', attrs=['bold'])} won the battle!")
+                            else:
+                                print(f"It's a tie")
+                            print(f"\nPlayer 1 Overall Pokemon's Health: {colored(total_player1_health, 'yellow', attrs=['bold', 'underline'])}")
+                            print(f"Player 2 Overall Pokemon's Health: {colored(total_player2_health, 'yellow', attrs=['bold', 'underline'])}")
+                            exit()
+                        else:
+                            print("\nAll of your pokemons must be used for you able to run!")
+                            input("\nPress any key to back: ")
+                            os.system("cls")
+                            battle_stats.last_frame()
                 else:
                     os.system("cls")
                     battle_stats.last_frame()
@@ -145,16 +191,21 @@ while True:
         battle_stats.player1_health_increase(5)
         battle_stats.player2_health_decrease(10)
         battle_stats.fatigue(2)
+        round_history.append("Player 1 Won")
     elif battle_stats.player1_pokemon.power < battle_stats.player2_pokemon.power:
         battle.player2_won()
         battle_stats.player2_health_increase(5)
         battle_stats.player1_health_decrease(10)
         battle_stats.fatigue(2)
+        round_history.append("Player 2 Won")
     else:
         battle.tie()
         battle_stats.fatigue(2)
+        round_history.append("Tie")
     battle.reset()
     if player1_temporary_buff:
         battle.player1_pokemon.power -= 10
     if player2_temporary_buff:
         battle.player2_pokemon.power -= 10
+    i += 1
+    history.append(round_history)
